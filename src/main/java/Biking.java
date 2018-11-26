37dc6ad6a361aab1f4f44e0d7ac839ecbd388610
alysiareyes <alysia.reyes@sjsu.edu>
2018-11-25 23:28:41 -0800

    Update Biking.java

--- src/main/java/Biking.java
+++ src/main/java/Biking.java
@@ -1,13 +1,13 @@
 /**
- *
+ * Class Biking determines the amount of time it would take for a person to bike to the determined destination
  */
 public class Biking extends Transportation {
     private final static int TRAVEL_SEC_PER_BREAK = 60*60;
     private final static int BREAK_TIME_MIN = 15;
 
     /**
-     *
-     * @param durationInSec
+     * 
+     * @param durationInSec how long it would take to bike to the given destionation
      */
     public Biking(int durationInSec){
         super(durationInSec);
@@ -15,7 +15,7 @@ public class Biking extends Transportation {
 
     /**
      *
-     * @return
+     * @return returns the estimated time of travel
      */
     protected int calculateBreakTime() {
         double numOfBreak = Math.floor(durationInSec/ TRAVEL_SEC_PER_BREAK);
@@ -25,14 +25,14 @@ public class Biking extends Transportation {
 
     /**
      *
-     * @return
+     * @return returns 
      */
     public String toString(){return TransportationFactory.BIKING_TYPE;}
 
     /**
      *
-     * @param other
-     * @return
+     * @param other 
+     * @return 
      */
     public boolean equals(Object other){
         return(other.getClass().equals(this));
